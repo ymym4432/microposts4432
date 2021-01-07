d63baca9cb5740dd9cc7df7985f3b247268f3c68
yoshimi.tanaka <ymym4432@au.com>
2021-01-07 07:33:31 +0000

    top page

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,6 @@
 
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+Route::get('/', function () {
+    return view('welcome');
+});
