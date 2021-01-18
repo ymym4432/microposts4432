afc733241b49004b025d2517a815905c412eefe5
yoshimi.tanaka <ymym4432@au.com>
2021-01-18 08:29:01 +0000

    timeline

--- database/migrations/2021_01_13_160136_create_user_follow_table.php
+++ database/migrations/2021_01_13_160136_create_user_follow_table.php
@@ -15,15 +15,19 @@ class CreateUserFollowTable extends Migration
     {
         Schema::create('user_follow', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->integer('follow_id')->unsigned()->index();
             $table->timestamps();
+
+            // 外部キー設定
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('follow_id')->references('id')->on('users')->onDelete('cascade');
+
+            // user_idとfollow_idの組み合わせの重複を許さない
+            $table->unique(['user_id', 'follow_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('user_follow');
